3f98d13522dacc118dc5b34eb1162e4edd6b8596
rm <mbsgzz@126.com>
2022-04-22 22:52:32 +0800

    增加防火墙开放转发配置

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,3 +14,30 @@
 sed -i 's/192.168.1.1/172.16.1.1/g' package/base-files/files/bin/config_generate
 sed -i 's/0.openwrt.pool.ntp.org/time.pool.aliyun.com/g' package/base-files/files/bin/config_generate
 sed -i 's/1.openwrt.pool.ntp.org/ntp1.aliyun.com/g' package/base-files/files/bin/config_generate
+
+
+cat>>package/network/config/firewall/files/firewall.config<<EOF
+
+# forward web port 80 at 9666
+config redirect                      
+        option dest 'lan'               
+        option target 'DNAT'                    
+        option name 'route9666'                    
+        list proto 'tcp'              
+        option src 'wan'              
+        option src_dport '9666'                 
+        option dest_port '80'          
+        option dest_ip '172.16.1.1' 
+
+# forward ssh port 22 at 9222
+config redirect                        
+        option dest 'lan'                         
+        option target 'DNAT'         
+        option name 'ssh9222'               
+        list proto 'tcp'                    
+        option src 'wan'                       
+        option src_dport '9222'                 
+        option dest_ip '172.16.1.1'           
+        option dest_port '22'
+
+EOF
